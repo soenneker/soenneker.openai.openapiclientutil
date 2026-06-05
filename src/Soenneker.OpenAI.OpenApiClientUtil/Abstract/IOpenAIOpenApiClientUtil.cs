59aa6ffca2920e316faae7d0c966cf8eeb028e14
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:29 -0500

    xml commenting

--- src/Soenneker.OpenAI.OpenApiClientUtil/Abstract/IOpenAIOpenApiClientUtil.cs
+++ src/Soenneker.OpenAI.OpenApiClientUtil/Abstract/IOpenAIOpenApiClientUtil.cs
@@ -10,5 +10,10 @@ namespace Soenneker.OpenAI.OpenApiClientUtil.Abstract;
 /// </summary>
 public interface IOpenAIOpenApiClientUtil: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<OpenAIOpenApiClient> Get(CancellationToken cancellationToken = default);
 }
